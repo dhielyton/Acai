315935e32f362ca98619320b9167401968a1f05f
dhielyton <dhielyton@gmail.com>
2020-04-04 09:33:00 -0300

    alteração do acompanhamento. Inclusão dos testes.

--- src/Acai.Domain/Acompanhamento/Acompanhamento.cs
+++ src/Acai.Domain/Acompanhamento/Acompanhamento.cs
@@ -5,8 +5,20 @@ using System.Text;
 
 namespace Acai.Domain.Acompanhamento
 {
-    public class Acompanhamento: Entity
+    public class Acompanhamento : Produto.Produto, IPreco, ITempoPreparo
     {
-        public string Descricao { get; set; }
+        public Acompanhamento(string descricao, decimal preco , int minutosPreparo = 0) : base(descricao)
+        {
+            Preco = preco;
+            MinutosPreparo = minutosPreparo;
+        }
+
+        public decimal Preco { get; set; }
+        public int MinutosPreparo { get ; set ; }
+
+        public static Acompanhamento Create(string descricao, decimal preco = 0.00M, int minutosPreparo = 0)
+        {
+            return new Acompanhamento(descricao, preco, minutosPreparo);
+        }
     }
 }
---
+++ testes/Acai.UnitTest/Acompanhamento/AcompanhamentoTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Acai.Domain.Acompanhamento;
+using FluentAssertions;
+namespace Acai.UnitTest.Acompanhamento
+{
+    public class AcompanhamentoTest
+    {
+
+        [Fact]
+        public void CriarAcompanhamentoComSucesso()
+        {
+            var acompanhamento = Domain.Acompanhamento.Acompanhamento.Create("Granola");
+            acompanhamento.Should().NotBeNull();
+            acompanhamento.Descricao.Should().NotBeNullOrEmpty();
+            acompanhamento.Preco.Should().Be(0.00M);
+            acompanhamento.MinutosPreparo.Should().Be(0);
+
+        }
+
+        [Fact]
+        public void CriarAcompanhamentoComSucessoComPreco()
+        {
+            var acompanhamento = Domain.Acompanhamento.Acompanhamento.Create("Leite Ninho", 3.00M);
+            acompanhamento.Should().NotBeNull();
+            acompanhamento.Descricao.Should().NotBeNullOrEmpty();
+            acompanhamento.Preco.Should().Be(3.00M);
+            acompanhamento.MinutosPreparo.Should().Be(0);
+
+        }
+
+        [Fact]
+        public void CriarAcompanhamentoComSucessoComPrecoRTempoPreparo()
+        {
+            var acompanhamento = Domain.Acompanhamento.Acompanhamento.Create("Paçoca", 3.00M,3);
+            acompanhamento.Should().NotBeNull();
+            acompanhamento.Descricao.Should().NotBeNullOrEmpty();
+            acompanhamento.Preco.Should().Be(3.00M);
+            acompanhamento.MinutosPreparo.Should().Be(3);
+
+        }
+
+    }
+}
