16e3e2492a2f4aba9e65ff7ea044231f080dde35
dhielyton <dhielyton@gmail.com>
2020-04-05 12:46:19 -0300

    inclusão da camada de infrasctructrue.

---
+++ Acai.Infrastructure/EntityConfiguration/ProdutoTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Acai.Domain.Produto;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acai.Infrastructure.EntityConfiguration
+{
+    public class ProdutoTypeConfiguration : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+          
+        }
+    }
+}
