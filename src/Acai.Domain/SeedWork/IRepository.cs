9ea5773c2cf2435f297276126d5a4038bea88f1f
dhielyton <dhielyton@gmail.com>
2020-04-05 09:23:36 -0300

    inclusão da interface do repositorio.

---
+++ src/Acai.Domain/SeedWork/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acai.Domain.SeedWork
+{
+    public interface IRepository<T> where T : Entity
+    {
+
+        Task<T> Incluir(T entity);
+        Task<T> Editar(T entity);
+        Task Excluir(T Entity);
+        Task<T> ObterPorId(int id);
+        Task<ICollection<T>> ObterTodos();
+    }
+}
